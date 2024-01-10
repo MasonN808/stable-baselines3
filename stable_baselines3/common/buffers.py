4f9079ee400217c211b0172ed4cbdcfcfc25d3d1
MasonN808 <mason.naka@gmail.com>
2024-01-09 22:30:05 -1000

    changed to cost.any()

--- stable_baselines3/common/buffers.py
+++ stable_baselines3/common/buffers.py
@@ -520,7 +520,7 @@ class RolloutBuffer(BaseBuffer):
         self.episode_starts[self.pos] = np.array(episode_start)
         self.values[self.pos] = value.clone().cpu().numpy().flatten()
         self.log_probs[self.pos] = log_prob.clone().cpu().numpy()
-        if cost:
+        if cost.any():
             self.costs[self.pos] = np.array(cost)
         if value_cost:
             self.values_costs[self.pos] = value_cost.clone().cpu().numpy().flatten()
