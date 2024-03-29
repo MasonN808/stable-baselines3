e2326ff6f480aa8eb75b17084e3c04b41bc5842a
MasonN808 <mason.nakamura1@marist.edu>
2024-03-28 18:51:05 -0700

    Refactors and started CNN preprocessing for image observation with kinematics

--- stable_baselines3/common/torch_layers.py
+++ stable_baselines3/common/torch_layers.py
@@ -78,19 +78,9 @@ class LiDARCNN(BaseFeaturesExtractor):
             nn.Flatten(),
         )
 
-        # Compute shape by doing one forward pass
-        # with th.no_grad(): 
-        #     n_flatten = self.cnn(th.as_tensor(observation_space.sample()['lidar']).float()).shape[1]
-        
-        # This should be size of predefined MLP first layer architecture + number of combined features from kinematics
-        # self.linear = nn.Sequential(nn.Linear(features_dim + 12, features_dim + 12), nn.ReLU())
-
-        # self.flatten = nn.Flatten()
-
     def forward(self, observations: th.Tensor) -> th.Tensor:
         observed_kinematics_obs = observations['observation']
         goal_kinematics_obs = observations['observation']
-        # lidar_obs = observations['lidar'].permute(0, 2, 1) # Use permute [batch_size, channels, sequence_length] for Conv1d, since 'lidar' data is in the shape [batch_size, sequence_length, channels]
         lidar_obs = observations['lidar']
         # Process lidar observations through CNN and flatten the output
         cnn_out = self.cnn(lidar_obs)
@@ -101,14 +91,76 @@ class LiDARCNN(BaseFeaturesExtractor):
         
         # Concatenate the CNN output with kinematics observations
         combined_features = th.cat((cnn_out, observed_kinematics_obs, goal_kinematics_obs), dim=1)
-        # Pass the combined tensor through the linear layer(s)
         return combined_features
-        # return self.linear(combined_features)
 
 
 class NatureCNN(BaseFeaturesExtractor):
     """
     CNN from DQN Nature paper:
+        Mnih, Volodymyr, et al.
+        "Human-level control through deep reinforcement learning."
+        Nature 518.7540 (2015): 529-533.
+
+    :param observation_space:
+    :param features_dim: Number of features extracted.
+        This corresponds to the number of unit for the last layer.
+    :param normalized_image: Whether to assume that the image is already normalized
+        or not (this disables dtype and bounds checks): when True, it only checks that
+        the space is a Box and has 3 dimensions.
+        Otherwise, it checks that it has expected dtype (uint8) and bounds (values in [0, 255]).
+    """
+
+    def __init__(
+        self,
+        observation_space: gym.Space,
+        features_dim: int = 512,
+        normalized_image: bool = False,
+    ) -> None:
+        assert isinstance(observation_space, spaces.Box), (
+            "NatureCNN must be used with a gym.spaces.Box ",
+            f"observation space, not {observation_space}",
+        )
+        super().__init__(observation_space, features_dim)
+        assert isinstance(observation_space, spaces.Dict), (
+            "Image must be used with a gym.spaces.Dict ",
+            f"observation space, not {observation_space}",
+        )
+        # We assume CxHxW images (channels first)
+        # Re-ordering will be done by pre-preprocessing or wrapper
+        assert is_image_space(observation_space['image'], check_channels=False, normalized_image=normalized_image), (
+            "You should use NatureCNN "
+            f"only with images not with {observation_space}\n"
+            "(you are probably using `CnnPolicy` instead of `MlpPolicy` or `MultiInputPolicy`)\n"
+            "If you are using a custom environment,\n"
+            "please check it using our env checker:\n"
+            "https://stable-baselines3.readthedocs.io/en/master/common/env_checker.html.\n"
+            "If you are using `VecNormalize` or already normalized channel-first images "
+            "you should pass `normalize_images=False`: \n"
+            "https://stable-baselines3.readthedocs.io/en/master/guide/custom_env.html"
+        )
+        n_input_channels = observation_space.shape[0]
+        self.cnn = nn.Sequential(
+            nn.Conv2d(n_input_channels, 32, kernel_size=8, stride=4, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+
+        # Compute shape by doing one forward pass
+        with th.no_grad():
+            n_flatten = self.cnn(th.as_tensor(observation_space.sample()[None]).float()).shape[1]
+
+        self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.ReLU())
+
+    def forward(self, observations: th.Tensor) -> th.Tensor:
+        return self.linear(self.cnn(observations))
+
+class ConcatenatedNatureCNN(BaseFeaturesExtractor):
+    """
+    CNN from DQN Nature paper plus kinematics observation:
         Mnih, Volodymyr, et al.
         "Human-level control through deep reinforcement learning."
         Nature 518.7540 (2015): 529-533.
