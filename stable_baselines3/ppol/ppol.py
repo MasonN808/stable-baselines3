7f574fa83ba26a8d3cd97e0f3270d0730048a0cd
MasonN808 <mason.naka@gmail.com>
2024-04-02 12:02:28 -0400

    added - to objective function

--- stable_baselines3/ppol/ppol.py
+++ stable_baselines3/ppol/ppol.py
@@ -336,7 +336,7 @@ class PPOL(GeneralizedOnPolicyAlgorithm):
                     # PPO surrogate loss
                     ppo_loss = policy_loss + self.ent_coef * entropy_loss + self.vf_coef * (value_loss + cost_value_loss)
                     # Apply rescale to objective
-                    loss = (1/(1+th.sum(lambdas))) * (ppo_loss + th.sum(lambdas * (cost_values-d)))
+                    loss = (1/(1+th.sum(lambdas))) * (ppo_loss - th.sum(lambdas * (cost_values-d)))
 
                 else:
                     loss = policy_loss + self.ent_coef * entropy_loss + self.vf_coef * value_loss
